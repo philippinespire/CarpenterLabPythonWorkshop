6e3bd480a4ba3109764841cc697a81a11b60670a
Getterrobog <ilope002@odu.edu>
2020-05-28 15:36:54 -0400

    python commands sheet

--- myfirstdir/myfirstprogram.py
+++ myfirstdir/myfirstprogram.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 print('Hello world')
 name=input('What is your name?')
-print('Hi '+name +"I hope you are enjoyomg the workshop. Where's the coffee")
+print('Hi '+name +"I hope you are enjoyomg the workshop. Where's the coffee?")

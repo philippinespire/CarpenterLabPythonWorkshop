c52659dbcb36482d06007561580513bd3f83161a
Getterrobog <ilope002@odu.edu>
2020-06-04 14:11:18 -0400

    latest updates

--- myfirstdir/myfirstprogram.py
+++ myfirstdir/myfirstprogram.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 print('Hello world')
 name=input('What is your name?')
-print('Hi '+name +"I hope you are enjoyomg the workshop. Where's the coffee?")
+print('Hi '+name +"I hope you are enjoying the workshop. Where's the coffee?")
